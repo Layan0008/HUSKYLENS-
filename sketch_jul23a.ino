5965d7a33e68a3a4c96470c0b23617feecac5ad8
Layan <layan1005@hotmail.com>
2025-07-26 05:51:58 +0300

    Add files via upload

---
+++ sketch_jul23a.ino
@@ -0,0 +1,39 @@
+#include "HUSKYLENS.h"
+#include "SoftwareSerial.h"
+
+HUSKYLENS huskylens;
+SoftwareSerial mySerial(10, 11); // RX, TX
+
+void setup() {
+  Serial.begin(9600);
+  mySerial.begin(9600);
+  huskylens.begin(mySerial);
+  
+  if (!huskylens.knock()) {
+    Serial.println("HuskyLens not connected!");
+  } else {
+    Serial.println("HuskyLens ready!");
+  }
+
+  huskylens.writeAlgorithm(ALGORITHM_FACE_RECOGNITION); //    COLOR_RECOGNITION
+}
+
+void loop() {
+  if (!huskylens.request()) {
+    Serial.println("No data from HuskyLens");
+  } else if (!huskylens.available()) {
+    Serial.println("No object detected");
+  } else {
+    while (huskylens.available()) {
+      HUSKYLENSResult result = huskylens.read();
+      Serial.print("ID: ");
+      Serial.print(result.ID);
+      Serial.print("  X: ");
+      Serial.print(result.xCenter);
+      Serial.print("  Y: ");
+      Serial.println(result.yCenter);
+    }
+  }
+
+  delay(500);
+}
